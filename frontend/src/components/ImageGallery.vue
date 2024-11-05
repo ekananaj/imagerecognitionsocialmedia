cf8baddbfeb8318da348ff1969a2e6342a585385
ekananaj <ekananaj03@gmail.com>
2024-11-05 08:03:22 -0500

    Create ImageGallery.vue

---
+++ frontend/src/components/ImageGallery.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Uploaded Images</h2>
+    <ul>
+      <li v-for="(image, index) in images" :key="index">
+        <img :src="image.url" alt="Uploaded image" width="200" />
+        <p>{{ image.filterResult }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [] // replace with fetched image data
+    };
+  },
+  created() {
+    // Fetch images from the backend
+    fetch('http://localhost:8000/api/images')
+      .then(response => response.json())
+      .then(data => this.images = data)
+      .catch(error => console.error('Error:', error));
+  }
+};
+</script>
