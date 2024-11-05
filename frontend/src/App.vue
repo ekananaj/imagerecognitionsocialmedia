89b39a856e34f4d92fdaad56e738045d8a3d2f26
ekananaj <ekananaj03@gmail.com>
2024-11-05 08:02:07 -0500

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Image Recognition & Filtering App</h1>
+    <ImageUpload />
+    <ImageGallery />
+  </div>
+</template>
+
+<script>
+import ImageUpload from './components/ImageUpload.vue';
+import ImageGallery from './components/ImageGallery.vue';
+
+export default {
+  components: {
+    ImageUpload,
+    ImageGallery
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+}
+</style>
